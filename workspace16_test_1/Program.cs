b64d12399b0a4ed9c29129322d41c6fc928f8ec4
talisman <talisman@TALISMAN-PC>
2017-03-26 13:36:44 +0900

    プロジェクト ファイルを追加します。

---
+++ workspace16_test_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace workspace16_test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dat;
+            var vb = new XLWorkbook(@"C:\test.xlsx");
+            var cell = vb.Cell("A1");//A1を読み込み
+            dat = cell.Value.ToString();
+            vb.Cell("B2").Value = dat;//B2へ書き込み
+            vb.SaveAs(@"C:\testafter.xlsx");
+        }
+    }
+}
